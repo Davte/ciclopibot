d2e876cfadc57b47ea826446b9a2d704e4d5104a
Davte <Davte@users.noreply.github.com>
2019-07-19 12:04:47 +0200

    Data module documented

---
+++ ciclopibot/data/__init__.py
@@ -0,0 +1,19 @@
+"""This folder contains user data.
+
+Therefore, it must be .gitignored and excluded from python packaging.
+
+Examples of data files
+- `ciclopi.db`: bot SQLite database file
+- Info and erro logs
+- `config.py`: configuration file providing local host and port where web app
+    should run
+    ```python
+    local_host = '127.0.0.1'
+    port = 8080
+    ```
+- `help.json`: file where /help data are stored
+- `passwords.py`: secret file where you can store your bot token
+    ```python
+    bot_token = "111222333:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+    ```
+"""
