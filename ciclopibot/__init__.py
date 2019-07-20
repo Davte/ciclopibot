a581206d7cab95ad27c47c5dbe9c4fecccef68a8
Davte <Davte@users.noreply.github.com>
2019-07-20 10:54:20 +0200

    New version

--- ciclopibot/__init__.py
+++ ciclopibot/__init__.py
@@ -3,6 +3,6 @@
 __author__ = "Davide Testa"
 __email__ = "davide@davte.it"
 __license__ = "GNU General Public License v3.0"
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 __maintainer__ = "Davide Testa"
 __contact__ = "t.me/davte"
