9d2f9b06dc01e85dcdccd4358394f7d0e740fb87
Davte <davte@libero.it>
2020-05-03 16:00:01 +0200

    Helper module deprecated (use davtelepot.helper instead), typos and minor refactoring

--- ciclopibot/__init__.py
+++ ciclopibot/__init__.py
@@ -3,7 +3,7 @@
 __author__ = "Davide Testa"
 __email__ = "davide@davte.it"
 __license__ = "GNU General Public License v3.0"
-__version__ = "1.1.16"
+__version__ = "1.1.17"
 __maintainer__ = "Davide Testa"
 __contact__ = "t.me/davte"
 
