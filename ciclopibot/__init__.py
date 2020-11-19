3cf0c06ba2c80a3bdce82d2608a4815dce61ac4a
Davte <davte@libero.it>
2020-11-19 14:29:48 +0100

    Items in __all__ must be str, not module

--- ciclopibot/__init__.py
+++ ciclopibot/__init__.py
@@ -3,10 +3,10 @@
 __author__ = "Davide Testa"
 __email__ = "davide@davte.it"
 __license__ = "GNU General Public License v3.0"
-__version__ = "1.2.3"
+__version__ = "1.2.4"
 __maintainer__ = "Davide Testa"
 __contact__ = "t.me/davte"
 
-from . import ciclopi
+from . import ciclopi, messages
 
-__all__ = [ciclopi]
+__all__ = ['ciclopi', 'messages']
